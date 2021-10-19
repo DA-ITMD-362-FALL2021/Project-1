f8cf562940b02a3f8ac15b159819597b38ad3de1
Deepan <deepan.acharya2015@gmail.com>
2021-10-19 17:59:37 -0500

    updated margins on the labels
    updated margins on the labels

--- js/script.js
+++ js/script.js
@@ -10,12 +10,12 @@ function btnClick(){
   b.style.backgroundColor = 'black';
   b.style.color = 'white';
 	
-  var name = document.getElementsByName('fname_field')[0].value + ' ' + document.getElementsByName('lname_field')[0].value;
-  alert('The account for subscriber ' + name + ' has been created');
+  var name = document.getElementsByName('fname')[0].value + ' ' + document.getElementsByName('lname')[0].value;
+  alert('You have successfully subscribed to the newsletter!');
 }
 
 function radiofocus(i){
-  document.getElementsByTagName('label')[i+4].style.color = 'orange';
+  document.getElementsByTagName('label')[i+4].style.color = 'grey';
 }
 
 function radioblur(i){
@@ -25,12 +25,12 @@ function radioblur(i){
 window.addEventListener('load', jschg);
 b.addEventListener('click', btnClick);
 
-document.getElementsByName('news_field')[0].addEventListener('focus', function(){radiofocus(0);});
-document.getElementsByName('news_field')[1].addEventListener('focus', function(){radiofocus(1);});
-document.getElementsByName('news_field')[2].addEventListener('focus', function(){radiofocus(2);});
-document.getElementsByName('news_field')[3].addEventListener('focus', function(){radiofocus(3);});
+document.getElementsByName('sms')[0].addEventListener('focus', function(){radiofocus(0);});
+document.getElementsByName('sms')[1].addEventListener('focus', function(){radiofocus(1);});
+document.getElementsByName('sms')[2].addEventListener('focus', function(){radiofocus(2);});
+document.getElementsByName('sms')[3].addEventListener('focus', function(){radiofocus(3);});
 
-document.getElementsByName('news_field')[0].addEventListener('blur', function(){radioblur(0);});
-document.getElementsByName('news_field')[1].addEventListener('blur', function(){radioblur(1);});
-document.getElementsByName('news_field')[2].addEventListener('blur', function(){radioblur(2);});
-document.getElementsByName('news_field')[3].addEventListener('blur', function(){radioblur(3);});
+document.getElementsByName('sms')[0].addEventListener('blur', function(){radioblur(0);});
+document.getElementsByName('sms')[1].addEventListener('blur', function(){radioblur(1);});
+document.getElementsByName('sms')[2].addEventListener('blur', function(){radioblur(2);});
+document.getElementsByName('sms')[3].addEventListener('blur', function(){radioblur(3);});
